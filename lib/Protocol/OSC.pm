0e4f3b8d7242216a562032d5efdb02d534bc56f3
vividsnow <vividsnow@gmail.com>
2013-06-13 03:22:47 +0400

    fix pod and test

--- lib/Protocol/OSC.pm
+++ lib/Protocol/OSC.pm
@@ -1,91 +1,92 @@
+# ABSTRACT: Open Sound Control v1.1 protocol implementation
+package Protocol::OSC;
 use strict;
 use warnings;
-# ABSTRACT: Open Sound Control v1.1 protocol implementation
-
-package Protocol::OSC {
-    use Scalar::Util 'looks_like_number';
-    use constant { NTP_EPOCH_DIFF => 2208988800, MAX_INT => 1 << 32 };
-    my %converter = qw(i N f f> d d> s Z*x!4 b N/C*x!4 h h t N2);
-    sub new { bless {
-        scheduler => sub { $_[0]->(splice @_, 1) },
-        actions => {},
-        splice(@_, 1),
-    }, shift }
-    sub parse {
-        my ($self, $data) = @_;
-        if ((my $f = substr $data, 0, 1) eq '#') { # bundle
-            my (undef, $time, $fraction, @msgs) = unpack 'Z8N2(N/A*)*', $data;
-            Protocol::OSC::Bundle->new($self->tag2time($time, $fraction), map $self->parse($_), @msgs);
-        } elsif ($f eq '/') { # message
-            my ($path, $type, $args) = unpack '(Z*x!4)2A*', $data;
-            substr $type, 0, 1, '';
-            Protocol::OSC::Message->new( $path, $type,
-                unpack join('', map $converter{$_} // (), split //, $type), pack 'Z*x!4', $args );
-        } else { warn 'broken osc packet' }
-    }
-    sub bundle {
-        my ($self, $time, @msgs) = @_;
-        pack 'Z8N2(N/A*)*', '#bundle', $self->time2tag($time), map {
-            $self->${\( defined $_->[0] && !looks_like_number $_->[0] ? 'message' : 'bundle' )}(@{$_})
-        } @msgs;
-    }
-    sub message {
-        my ($self, $path, $type, @args) = @_;
-        pack '(Z*x!4)2A*', $path, ','.$type,
-            join '', map pack($converter{$_}, shift@args),
-            grep exists$converter{$_}, split //, $type;
-    }
-    sub process {
-        my ($self, $packet, $scheduler_cb, $at_time, @bundle) = @_;
-        if ((my $r = ref$packet) eq 'Protocol::OSC::Bundle') {
-            map $self->process($_, $scheduler_cb, $packet->[0], $packet, @bundle), splice @$packet, 1;
-        } elsif ($r eq 'Protocol::OSC::Message') {
-            map {
-                ( $scheduler_cb || $self->{scheduler} )->($_->[1], $at_time, $_->[0], $packet, @bundle)
-            } $self->match($packet->[0]);
-        } else { $self->process($self->parse($packet), $scheduler_cb, $at_time, @bundle) }
-    }
-    sub actions { $_[0]->{actions} }
-    sub set_cb { $_[0]->{actions}->{$_[1]} = $_[2] }
-    sub del_cb { delete $_[0]->{actions}->{$_[1]} }
-    sub match {
-        my ($self, $pattern) = @_;
-        $pattern =~ s!(\*|//)!.+!g;
-        $pattern =~ y/?{},!/.()^|/;
-        map [$_, $self->{actions}->{$_}], grep /^$pattern$/, keys$self->{actions};
-    }
-    sub tag2time {
-        my ($self, $secs, $frac) = @_;
-        return undef if !$secs && $frac == 1;
-        $secs - NTP_EPOCH_DIFF + $frac / MAX_INT;
-    }
-    sub time2tag {
-        my ($self, $t) = @_;
-        return (0, 1) unless $t;
-        my $secs = int($t);
-        ( $secs + NTP_EPOCH_DIFF, int MAX_INT * ($t - $secs) );
-    }
-    sub to_stream { pack 'N/A*' => $_[1] }
-    sub from_stream {
-        my ($self, $buf) = @_;
-        return $buf if length $buf < 4;
-        my $n = unpack 'N', substr $buf, 0, 4;
-        return $buf if length $buf < $n + 4;
-        (unpack('N/A*', substr $buf, 0, 4+$n, ''), $buf);
-    }
+use Scalar::Util 'looks_like_number';
+use constant { NTP_EPOCH_DIFF => 2208988800, MAX_INT => 1 << 32 };
+my %converter = qw(i N f f> d d> s Z*x!4 b N/C*x!4 h h t N2);
+sub new { bless {
+    scheduler => sub { $_[0]->(splice @_, 1) },
+    actions => {},
+    splice(@_, 1),
+}, shift }
+sub parse {
+    my ($self, $data) = @_;
+    if ((my $f = substr $data, 0, 1) eq '#') { # bundle
+        my (undef, $time, $fraction, @msgs) = unpack 'Z8N2(N/A*)*', $data;
+        Protocol::OSC::Bundle->new($self->tag2time($time, $fraction), map $self->parse($_), @msgs);
+    } elsif ($f eq '/') { # message
+        my ($path, $type, $args) = unpack '(Z*x!4)2A*', $data;
+        substr $type, 0, 1, '';
+        Protocol::OSC::Message->new( $path, $type,
+            unpack join('', map $converter{$_} // (), split //, $type), pack 'Z*x!4', $args );
+    } else { warn 'broken osc packet' }
+}
+sub bundle {
+    my ($self, $time, @msgs) = @_;
+    pack 'Z8N2(N/A*)*', '#bundle', $self->time2tag($time), map {
+        $self->${\( defined $_->[0] && !looks_like_number $_->[0] ? 'message' : 'bundle' )}(@{$_})
+    } @msgs;
+}
+*msg = \&message;
+sub message {
+    my ($self, $path, $type, @args) = @_;
+    pack '(Z*x!4)2A*', $path, ','.$type,
+        join '', map pack($converter{$_}, shift@args),
+        grep exists$converter{$_}, split //, $type;
+}
+sub process {
+    my ($self, $packet, $scheduler_cb, $at_time, @bundle) = @_;
+    if ((my $r = ref$packet) eq 'Protocol::OSC::Bundle') {
+        map $self->process($_, $scheduler_cb, $packet->[0], $packet, @bundle), splice @$packet, 1;
+    } elsif ($r eq 'Protocol::OSC::Message') {
+        map {
+            ( $scheduler_cb || $self->{scheduler} )->($_->[1], $at_time, $_->[0], $packet, @bundle)
+        } $self->match($packet->[0]);
+    } else { $self->process($self->parse($packet), $scheduler_cb, $at_time, @bundle) }
+}
+sub actions { $_[0]->{actions} }
+sub set_cb { $_[0]->{actions}->{$_[1]} = $_[2] }
+sub del_cb { delete $_[0]->{actions}->{$_[1]} }
+sub match {
+    my ($self, $pattern) = @_;
+    $pattern =~ s!(\*|//)!.+!g;
+    $pattern =~ y/?{},!/.()^|/;
+    map [$_, $self->{actions}->{$_}], grep /^$pattern$/, keys$self->{actions};
+}
+sub tag2time {
+    my ($self, $secs, $frac) = @_;
+    return undef if !$secs && $frac == 1;
+    $secs - NTP_EPOCH_DIFF + $frac / MAX_INT;
+}
+sub time2tag {
+    my ($self, $t) = @_;
+    return (0, 1) unless $t;
+    my $secs = int($t);
+    ( $secs + NTP_EPOCH_DIFF, int MAX_INT * ($t - $secs) );
+}
+sub to_stream { pack 'N/A*' => $_[1] }
+sub from_stream {
+    my ($self, $buf) = @_;
+    return $buf if length $buf < 4;
+    my $n = unpack 'N', substr $buf, 0, 4;
+    return $buf if length $buf < $n + 4;
+    (unpack('N/A*', substr $buf, 0, 4+$n, ''), $buf);
 }
 
-package Protocol::OSC::Message {
-    sub new { bless [splice(@_,1)], shift }
-    sub path { $_[0]->[0] }
-    sub type { $_[0]->[1] }
-    sub args { my $self = shift; @$self[2..$#$self] }
-}
+package Protocol::OSC::Message;
+use strict;
+use warnings;
+sub new { bless [splice(@_,1)], shift }
+sub path { $_[0]->[0] }
+sub type { $_[0]->[1] }
+sub args { my $self = shift; @$self[2..$#$self] }
 
-package Protocol::OSC::Bundle {
-    sub new { bless [splice(@_,1)], shift }
-    sub time { $_[0]->[0] }
-    sub packets { my $self = shift; @$self[1..$#$self] }
-}
+package Protocol::OSC::Bundle;
+use strict;
+use warnings;
+sub new { bless [splice(@_,1)], shift }
+sub time { $_[0]->[0] }
+sub packets { my $self = shift; @$self[1..$#$self] }
 
 1;
--- t/use.t
+++ t/use.t
@@ -7,25 +7,27 @@ use Time::HiRes 'time';
 
 BEGIN { use_ok 'Protocol::OSC' }
 my $p = Protocol::OSC->new;
-my $port = empty_port(undef, 'udp');
 my @spec = (time,[qw(/echo isf 3 aaba 3.1)],[qw(/echo ii 3 1)]);
 
-my $in = IO::Socket::INET->new( qw(LocalAddr localhost LocalPort), $port, qw(Proto udp Type), SOCK_DGRAM ) || die $!;
-$in->blocking(0);
-my $client = IO::Socket::INET->new( qw(PeerAddr localhost PeerPort), $port, qw(Proto udp Type), SOCK_DGRAM ) || die $!;
-$client->send($p->bundle(@spec));
-$in->recv(my $packet, $in->sockopt(SO_RCVBUF));
+if (my $port = empty_port(undef, 'udp')) {
+    my $in = IO::Socket::INET->new( qw(LocalAddr localhost LocalPort), $port, qw(Proto udp Type), SOCK_DGRAM );
+    $in->blocking(0);
+    my $client = IO::Socket::INET->new( qw(PeerAddr localhost PeerPort), $port, qw(Proto udp Type), SOCK_DGRAM );
+    $client->send($p->bundle(@spec));
+    $in->recv(my $packet, $in->sockopt(SO_RCVBUF));
 
-ok $p->parse($packet)->[0] eq $spec[0], 'bundle in-out - udp';
+    ok $p->parse($packet)->[0] eq $spec[0], 'bundle in-out - udp' if $packet;
+}
 
-$port = empty_port(undef, 'tcp');
-$in = IO::Socket::INET->new( qw(LocalAddr localhost LocalPort), $port, qw(Proto tcp Type), SOCK_STREAM, qw(Listen 1 Reuse 1) ) || die $!;
-$in->blocking(0);
-$client = IO::Socket::INET->new( qw(PeerAddr localhost PeerPort), $port, Proto => 'tcp', Type => SOCK_STREAM ) || die $!;
-$client->send($p->to_stream($p->bundle(@spec)));
-$in->accept->recv($packet = undef, $in->sockopt(SO_RCVBUF));
-
-ok $p->parse(($p->from_stream($packet))[0])->[0] eq $spec[0], 'bundle in-out - tcp';
+if (my $port = empty_port(undef, 'tcp')) {
+    my $in = IO::Socket::INET->new( qw(LocalAddr localhost LocalPort), $port, qw(Proto tcp Type), SOCK_STREAM, qw(Listen 1 Reuse 1) );
+    $in->blocking(0);
+    my $client = IO::Socket::INET->new( qw(PeerAddr localhost PeerPort), $port, Proto => 'tcp', Type => SOCK_STREAM );
+    $client->send($p->to_stream($p->bundle(@spec)));
+    $in->accept->recv(my $packet, $in->sockopt(SO_RCVBUF));
+    
+    ok $p->parse(($p->from_stream($packet))[0])->[0] eq $spec[0], 'bundle in-out - tcp' if $packet;
+}
 
 $p->set_cb('/echo', sub { 
     ok !defined($_[0]), 'process 1';
